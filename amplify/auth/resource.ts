dd8b38d5dff1fc8ecd42815ba320e7cda2b7c558
NexWan <lcontrerasmartinez235@gmail.com>
2026-03-22 06:56:01 -0600

    chore: (temp) remove defineauth to trigger redeploy

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,21 +5,3 @@ import { postConfirmation } from "./post-confirmation/resource";
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-  userAttributes: {
-    preferredUsername: {
-      required: true,
-    },
-    givenName: {
-      required: false,
-    },
-  },
-  groups: ["admin", "guest"],
-  triggers: {
-    postConfirmation: postConfirmation,
-  },
-  access: (allow) => [allow.resource(postConfirmation).to(["addUserToGroup"])],
-});
